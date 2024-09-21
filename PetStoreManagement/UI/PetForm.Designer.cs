441e3385beabb159a0bf154f0f1d0e01bf9a8afc
soknem <povsoknem@gmail.com>
2024-09-21 18:34:51 +0700

    Auto commit

--- PetStoreManagement/UI/PetForm.Designer.cs
+++ PetStoreManagement/UI/PetForm.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(839, 450);
+            this.ClientSize = new System.Drawing.Size(860, 450);
             this.Name = "PetForm";
             this.Text = "PetForm";
             this.ResumeLayout(false);
